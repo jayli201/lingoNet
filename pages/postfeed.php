a32983bb85117e56a434f557a63c2a51b50972d7
Rebecca <rmzhou99@gmail.com>
2021-04-07 01:05:21 -0400

    remove duplicates for multiple languages in postfeed

--- pages/postfeed.php
+++ pages/postfeed.php
@@ -7,6 +7,7 @@
 ***************************************************************************************/ -->
 <?php
 require("../sql/postfeed_sql.php");
+require("../sql/profile_sql.php");
 $user_info_array = getPostfeedInfo();
 $arr_len = count($user_info_array);
 
@@ -67,135 +68,148 @@ if (isset($_GET['email'])) {
           <br />
 
           <?php $count = 0; ?>
+          <?php $displayed_users = array() ?>
           <?php foreach ($user_info_array as $key => $value) : ?>
+            <?php $language_info = getMultiLanguageInfo(json_decode($value)->email); ?>
+            <?php if (!in_array($language_info, $displayed_users)) : ?>
+              <?php array_push($displayed_users, $language_info); ?>
+              <?php if ($count % 3 == 0) : ?>
+                <div class="card-deck">
+                <?php endif; ?>
 
-            <?php if ($count % 3 == 0) : ?>
-              <div class="card-deck">
-              <?php endif; ?>
+                <div class="card border border-purple">
+                  <div class="card-body">
+                    <h5 class="card-title">
 
-              <div class="card border border-purple">
-                <div class="card-body">
-                  <h5 class="card-title">
+                      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                        <?= json_decode($value)->firstName ?>
+                        <?= json_decode($value)->lastName ?>
+                        <input type="hidden" name="firstName" id="firstName" value="<?= json_decode($value)->firstName ?>" />
+                        <input type="hidden" name="lastName" id="lastName" value="<?= json_decode($value)->lastName ?>" />
+                        <input type="hidden" name="friendEmail" id="friendEmail" value="<?= json_decode($value)->email ?>" />
 
-                    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-                      <?= json_decode($value)->firstName ?>
-                      <?= json_decode($value)->lastName ?>
-                      <input type="hidden" name="firstName" id="firstName" value="<?= json_decode($value)->firstName ?>" />
-                      <input type="hidden" name="lastName" id="lastName" value="<?= json_decode($value)->lastName ?>" />
-                      <input type="hidden" name="friendEmail" id="friendEmail" value="<?= json_decode($value)->email ?>" />
+                        <!-- if pending (outgoing), display right arrow icon -->
+                        <?php if (isPendingFriend($_SESSION['email'], json_decode($value)->email)) : ?>
+                          <!-- https://icons.getbootstrap.com/icons/arrow-right/ -->
+                          <button type="button" disabled name="pending" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Request Sent">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                            </svg>
+                          </button>
 
-                      <!-- if pending (outgoing), display right arrow icon -->
-                      <?php if (isPendingFriend($_SESSION['email'], json_decode($value)->email)) : ?>
-                        <!-- https://icons.getbootstrap.com/icons/arrow-right/ -->
-                        <button type="button" disabled name="pending" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Request Sent">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                          </svg>
-                        </button>
+                          <!-- if incoming, display left arrow icon -->
+                        <?php elseif (isPendingFriend(json_decode($value)->email, $_SESSION['email'])) : ?>
+                          <!-- https://icons.getbootstrap.com/icons/arrow-left/ -->
+                          <button type="button" disabled name="incoming" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Incoming Friend Request">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                            </svg>
+                          </button>
 
-                        <!-- if incoming, display left arrow icon -->
-                      <?php elseif (isPendingFriend(json_decode($value)->email, $_SESSION['email'])) : ?>
-                        <!-- https://icons.getbootstrap.com/icons/arrow-left/ -->
-                        <button type="button" disabled name="incoming" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Incoming Friend Request">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                          </svg>
-                        </button>
+                          <!-- if in friend table, display accepted icon  -->
+                        <?php elseif (isAcceptedFriend($_SESSION['email'], json_decode($value)->email)) : ?>
+                          <!-- https://icons.getbootstrap.com/icons/person-check-fill/ -->
+                          <button type="button" disabled name="accepted" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Accepted Friend">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                              <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            </svg>
+                          </button>
 
-                        <!-- if in friend table, display accepted icon  -->
-                      <?php elseif (isAcceptedFriend($_SESSION['email'], json_decode($value)->email)) : ?>
-                        <!-- https://icons.getbootstrap.com/icons/person-check-fill/ -->
-                        <button type="button" disabled name="accepted" class="btn btn-purple btn-sm" data-toggle="Disabled tooltip" data-placement="bottom" title="Accepted Friend">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                          </svg>
-                        </button>
+                          <!-- else if not in pending or friend table, then display addFriend button -->
+                        <?php else : ?>
+                          <!-- Add friend button -->
+                          <button type="submit" name="addFriend" class="btn btn-purple btn-sm" data-toggle="tooltip" data-placement="bottom" title="Add Friend">
+                            <!-- https://icons.getbootstrap.com/icons/person-plus-fill/ -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                              <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                              <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                            </svg>
+                          </button>
+                        <?php endif; ?>
 
-                        <!-- else if not in pending or friend table, then display addFriend button -->
-                      <?php else : ?>
-                        <!-- Add friend button -->
-                        <button type="submit" name="addFriend" class="btn btn-purple btn-sm" data-toggle="tooltip" data-placement="bottom" title="Add Friend">
-                          <!-- https://icons.getbootstrap.com/icons/person-plus-fill/ -->
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                            <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
-                          </svg>
-                        </button>
-                      <?php endif; ?>
+                      </form>
+                    </h5>
+                    <h6 class="card-subtitle mb-2">Can speak: </h6>
 
-                    </form>
-                  </h5>
-                  <h6 class="card-subtitle mb-2">Can speak: </h6>
-                  <p><?= json_decode($value)->native ?></p>
+                    <?php foreach ($language_info['native'] as $native) { ?>
+                      <?php echo $native ?>
+                      <br />
+                    <?php } ?>
+                    <br />
 
-                  <h6 class="card-subtitle mb-2 ">Want to practice: </h6>
-                  <p><?= json_decode($value)->target ?></p>
-                </div>
-                <div class="card-footer">
-                  <form action="" method="get">
-                    <a href="<?php $_SERVER['PHP_SELF'] ?>?email=<?= json_decode($value)->email ?>" class="btn btn-purple btn-sm">
-                      More info
-                    </a>
+                    <h6 class="card-subtitle mb-2 ">Want to practice: </h6>
 
-                    <input type="hidden" id="show_modal" value="<?php echo isset($_GET['email']); ?>">
+                    <?php foreach ($language_info['target'] as $target) { ?>
+                      <?php echo $target ?>
+                      <br />
+                    <?php } ?>
+                  </div>
+                  <div class="card-footer">
+                    <form action="" method="get">
+                      <a href="<?php $_SERVER['PHP_SELF'] ?>?email=<?= json_decode($value)->email ?>" class="btn btn-purple btn-sm">
+                        More info
+                      </a>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="moreInfo" tabindex="-1" role="dialog" aria-labelledby="moreInfoTitle" aria-hidden="true">
-                      <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">
-                              <?php if ($more_info['email']) : ?>
-                                <?php echo $more_info['firstName'] . " " . $more_info['lastName']; ?>
-                              <?php else : ?>
-                                Currently no info :(
-                              <?php endif ?>
-                            </h5>
+                      <input type="hidden" id="show_modal" value="<?php echo isset($_GET['email']); ?>">
 
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                            <table class="table">
-                              <tbody>
+                      <!-- Modal -->
+                      <div class="modal fade" id="moreInfo" tabindex="-1" role="dialog" aria-labelledby="moreInfoTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLongTitle">
                                 <?php if ($more_info['email']) : ?>
-                                  <tr>
-                                    <td>Introduction</td>
-                                    <td><?php echo $more_info['introduction']; ?></td>
-                                  </tr>
-                                  <tr>
-                                    <td>Looking For</td>
-                                    <td><?php echo $more_info['lookingFor']; ?></td>
-                                  </tr>
-                                  <tr>
-                                    <td>Why You?</td>
-                                    <td><?php echo $more_info['whyYou']; ?></td>
-                                  </tr>
+                                  <?php echo $more_info['firstName'] . " " . $more_info['lastName']; ?>
                                 <?php else : ?>
-                                  Come back later!
+                                  Currently no info :(
                                 <?php endif ?>
-                              </tbody>
-                            </table>
-                          </div>
-                          <div class="modal-footer">
+                              </h5>
 
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <table class="table">
+                                <tbody>
+                                  <?php if ($more_info['email']) : ?>
+                                    <tr>
+                                      <td>Introduction</td>
+                                      <td><?php echo $more_info['introduction']; ?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>Looking For</td>
+                                      <td><?php echo $more_info['lookingFor']; ?></td>
+                                    </tr>
+                                    <tr>
+                                      <td>Why You?</td>
+                                      <td><?php echo $more_info['whyYou']; ?></td>
+                                    </tr>
+                                  <?php else : ?>
+                                    Come back later!
+                                  <?php endif ?>
+                                </tbody>
+                              </table>
+                            </div>
+                            <div class="modal-footer">
+
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </form>
+                    </form>
+                  </div>
                 </div>
-              </div>
 
-              <?php if (($count % 3 == 2) || $count == $arr_len - 1) : ?>
-              </div>
-              </br>
+                <?php if (($count % 3 == 2) || $count == $arr_len - 1) : ?>
+                </div>
+                </br>
+              <?php endif; ?>
+              <?php $count++ ?>
             <?php endif; ?>
-            <?php $count++ ?>
-          <?php endforeach; ?>
 
+          <?php endforeach; ?>
         </div>
       </div>
 
