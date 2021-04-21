b6b6fc53a24d5681f975d67212ddad7326d35937
Rebecca Zhou <rmzhou99@gmail.com>
2021-04-21 00:07:07 -0400

    initialize card class

---
+++ angular/lingoNet-angular/src/app/card.ts
@@ -0,0 +1,9 @@
+export class Card {
+   constructor(
+      public first_name: string,
+      public last_name: string,
+      public email: string,
+      public native: [string],
+      public target: [string],
+   ){}
+}
